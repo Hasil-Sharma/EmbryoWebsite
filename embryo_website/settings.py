83d8796afcf6716600b3c2b4a434f52ab9762f1a
Rohan Badlani <rohan.badlani@gmail.com>
2014-07-25 23:13:04 +0530

    Running Instructions for the website added. TODO: Admin interface instructions.

--- embryo_website/settings.py
+++ embryo_website/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME':  'Embryo_test',                    # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3
-        'PASSWORD': 'hasil',                  # Not used with sqlite3.
+        'PASSWORD': 'rohan',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
